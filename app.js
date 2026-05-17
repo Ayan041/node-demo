938dfe70152c62194aa154b94d12ec59308cf26b
Ayan041 <140093089+Ayan041@users.noreply.github.com>
2026-05-17 07:54:52 +0530

    Fixed about page response

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
     req.setEncoding("hello node.js app.");
 })
+
 app.get("/about", (req, res) => {
-    console.log("About page.");
+    console.log("About Us page fixed.");
 })
 
 app.listen(5000, () => {
