c667db145f03b5d0cb7b05441608941c97336873
Ayan041 <140093089+Ayan041@users.noreply.github.com>
2026-05-17 07:46:54 +0530

    Added about route

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
     req.setEncoding("hello node.js app.");
 })
+app.get("/about", (req, res) => {
+    console.log("About page.");
+})
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000.");
